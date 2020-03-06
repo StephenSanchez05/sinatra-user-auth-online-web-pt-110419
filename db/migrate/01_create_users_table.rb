37efb8197dc322c76ddf02f5323c123acd185f1c
Stephen Sanchez <stephensanchez05@gmail.com>
2020-03-06 23:36:09 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -1,9 +1,9 @@
-class def CreateUsersTable < ActiveRecord::Migration
+class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password
     end
-  end 
+  end
 end
